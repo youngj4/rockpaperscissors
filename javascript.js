288848c885d30db78d9f45b7545ea1364aadfc5a
youngj4 <john@rapidrecon.com>
2024-08-19 16:13:30 -0700

    add human and computer score variables

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
 // console.log("Hello world!")
 
+const humanScore = 0;
+const computerScore = 0;
+
 function getComputerChoice() {
     let result = Math.floor(Math.random() * 3);
     if (result === 0) {
