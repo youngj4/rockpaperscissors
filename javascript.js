bac46713c441bc9900f40414d2c1b4443f67d1f1
youngj4 <john@rapidrecon.com>
2024-08-19 15:56:09 -0700

    add function to randmly generate computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
-console.log("Hello world!")
+// console.log("Hello world!")
+
+function getComputerChoice() {
+    let result = Math.floor(Math.random() * 3);
+    if (result === 0) {
+        return "Rock"
+    }
+    else if (result === 1) {
+        return "Paper"
+    }
+    else {
+        return "Scissors"
+    }
+}
